3d7aea1b4702e931064101a9e04b91e2cd3312c2
Reece Williams <reecepbcups@gmail.com>
2022-09-12 15:08:19 -0500

    better test_node.sh

--- test_node.sh
+++ test_node.sh
@@ -1,6 +1,7 @@
 # Ensure eve is installed first.
 
 KEY="wba1"
+KEY2="wba2"
 CHAINID="wba-t1"
 MONIKER="localeve"
 KEYALGO="secp256k1"
@@ -17,16 +18,19 @@ command -v jq > /dev/null 2>&1 || { echo >&2 "jq not installed. More info: https
 
 from_scratch () {
 
-  # make install
+  echo "Building WBA executable from ./build.sh file"
+  sh build.sh # some have issues with 'make install'
 
   # remove existing daemon
   rm -rf ~/.wbad/* 
-
-  # if $KEY exists it should be deleted
-  # decorate bright ozone fork gallery riot bus exhaust worth way bone indoor calm squirrel merry zero scheme cotton until shop any excess stage laundry
+    
+  # Create 2 accounts from the KEY variables (only the first is a validator, DO NOT USE THESE FOR ANYTHING OTHER THAN TESTING)
   # wba1hj5fveer5cjtn4wd6wstzugjfdxzl0xpcfe228
   echo "decorate bright ozone fork gallery riot bus exhaust worth way bone indoor calm squirrel merry zero scheme cotton until shop any excess stage laundry" | wbad keys add $KEY --keyring-backend $KEYRING --algo $KEYALGO --recover
-  # Set moniker and chain-id for Craft
+  # wba13unas8vt8lz8dzyxhqjc8kw54423qm50ve9hp9
+  echo "suggest spike coral earth bring smart again entry quick setup alpha help ahead program give cup fashion pact citizen three wave harsh run ramp" | wbad keys add $KEY2 --keyring-backend $KEYRING --algo $KEYALGO --recover
+
+  # Set moniker and chain-id for Wba
   wbad init $MONIKER --chain-id $CHAINID 
 
   # Function updates the config based on a jq argument as a string
@@ -37,25 +41,20 @@ from_scratch () {
 
   # Set gas limit in genesis
   update_test_genesis '.consensus_params["block"]["max_gas"]="100000000"'
-  update_test_genesis '.app_state["gov"]["voting_params"]["voting_period"]="15s"'
-
-  # Change chain options to use EXP as the staking denom for craft
+  update_test_genesis '.app_state["gov"]["voting_params"]["voting_period"]="15s"'  
   update_test_genesis '.app_state["staking"]["params"]["bond_denom"]="uwba"'
-  # update_test_genesis '.app_state["bank"]["params"]["send_enabled"]=[{"denom": "ueve","enabled": false}]'
-  update_test_genesis '.app_state["staking"]["params"]["min_commission_rate"]="0.100000000000000000"'
-
-  # update from token -> ucraft
+  # update_test_genesis '.app_state["bank"]["params"]["send_enabled"]=[{"denom": "uwbanottransferable","enabled": false}]'
+  update_test_genesis '.app_state["staking"]["params"]["min_commission_rate"]="0.050000000000000000"'
   update_test_genesis '.app_state["mint"]["params"]["mint_denom"]="uwba"'  
   update_test_genesis '.app_state["gov"]["deposit_params"]["min_deposit"]=[{"denom": "uwba","amount": "1000000"}]'
   update_test_genesis '.app_state["crisis"]["constant_fee"]={"denom": "uwba","amount": "1000"}'
 
 
-  # Allocate genesis accounts
-  # 10 eve (1 of which is used for validator)
-  wbad add-genesis-account $KEY 10000000uwba --keyring-backend $KEYRING
-  # wbad add-genesis-account $KEY 100uwba,100utoken2 --keyring-backend $KEYRING --append
+  # Allocate genesis accounts, first is validator  
+  wbad add-genesis-account $KEY 1000000000uwba --keyring-backend $KEYRING
+  wbad add-genesis-account $KEY2 100000000uwba,100uowba2token --keyring-backend $KEYRING  
 
-  # create gentx with 1 eve
+  # create gentx with 1 wba
   wbad gentx $KEY 10000000uwba --keyring-backend $KEYRING --chain-id $CHAINID
 
   # Collect genesis tx
@@ -70,7 +69,6 @@ from_scratch
 # Opens the RPC endpoint to outside connections
 sed -i '/laddr = "tcp:\/\/127.0.0.1:26657"/c\laddr = "tcp:\/\/0.0.0.0:26657"' ~/.wbad/config/config.toml
 sed -i 's/cors_allowed_origins = \[\]/cors_allowed_origins = \["\*"\]/g' ~/.wbad/config/config.toml
-# cors_allowed_origins = []
 
-# # Start the node (remove the --pruning=nothing flag if historical queries are not needed)
-wbad start --pruning=nothing  --minimum-gas-prices=0uwba #--mode validator     
+# Start the node (remove the --pruning=nothing flag if historical queries are not needed)
+wbad start --pruning=nothing  --minimum-gas-prices=0uwba
